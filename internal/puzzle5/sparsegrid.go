8cdcd45bdb42c1178ecd8f6d7055ea7a1c2dd66e
Kristoffer Östlund <kristoffer@instabox.se>
2021-12-30 17:33:53 +0100

    Tighten up line drawing for sparse grid

--- internal/puzzle5/sparsegrid.go
+++ internal/puzzle5/sparsegrid.go
@@ -37,40 +37,22 @@ func (sg *SparseGrid) addLine(v Vector) {
 	fromY, toY := v[0].Y, v[1].Y
 	fromX, toX := v[0].X, v[1].X
 
-	if numutil.Float64sMatch(slope, 0) {
-		sg.addStaightLine(v)
-		return
-	}
-
-	incX := 1
-	compX := func(x int) bool { return x <= toX }
-	if fromX > toX {
-		compX = func(x int) bool { return x >= toX }
-		incX = -1
-	}
-	incY := 1
-	compY := func(y int) bool { return y <= toY }
-	if fromY > toY {
-		compY = func(y int) bool { return y >= toY }
-		incY = -1
-	}
+	incX, compX := loopDirection(fromX, toX)
+	incY, compY := loopDirection(fromY, toY)
 
 	for x, y := fromX, fromY; compX(x) && compY(y); x, y = x+incX, y+incY {
 		sg.add(Point{x, y})
 	}
 }
 
-func (sg *SparseGrid) addStaightLine(v Vector) {
-	fromY, toY := numutil.FromTo(v[0].Y, v[1].Y)
-	fromX, toX := numutil.FromTo(v[0].X, v[1].X)
-
-	inc := func(x, y int) (int, int) { return x + 1, y }
-	if fromX == toX {
-		inc = func(x, y int) (int, int) { return x, y + 1 }
-	}
-
-	for x, y := fromX, fromY; x <= toX && y <= toY; x, y = inc(x, y) {
-		sg.add(Point{x, y})
+func loopDirection(from, to int) (int, func(y int) bool) {
+	switch true {
+	case from < to:
+		return 1, func(v int) bool { return v <= to }
+	case from > to:
+		return -1, func(v int) bool { return v >= to }
+	default:
+		return 0, func(v int) bool { return v == to }
 	}
 }
 
